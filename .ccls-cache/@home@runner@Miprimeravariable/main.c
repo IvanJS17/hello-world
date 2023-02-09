6c002e470ff088a189eb642762e2ce4c3d4e970f
Ivan Juarez <63679095+IvanJS17@users.noreply.github.com>
2023-02-09 14:42:40 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Miprimeravariable/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() 
+{
+  int integerA = 1;
+  float  floatA  =  1.1;
+  double  doubleA  =  1.2;
+  char  letterA  =  'A';
+
+  //En la siguiente linea estamos imprimiendo mi variable integerA
+  printf("El valor de nuestro entera A es: %i \n", integerA);
+
+  printf("El valor de mi flotante A es: %f \n", floatA);
+
+  printf("El valor de mi double A es: %f \n", doubleA);
+
+  printf("El valor de mi caracter A es: %c \n", letterA);
+  
+  return 0;
+}
